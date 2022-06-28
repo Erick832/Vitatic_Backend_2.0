e6a89faf91765dc853f23528e103da3d0a018bcc
Erick <erick19arones@gmail.com>
2022-06-28 10:50:07 -0500

    Nueva actualización

---
+++ Vitatic.DTO/Request/DtoPriority.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vitatic.DTO.Request
+{
+    public class DtoPriority
+    {
+        public string? Priority { get; set; } 
+        public int ActivityId { get; set; }
+        public string? Category { get; set; }
+
+    }
+
+    
+
+ 
+
+}
+
---
+++ Vitatic.Entities/Activity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vitatic.Entities
+{
+    public class Activity:EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Priority { get; set; }   
+        public DateTime Date { get; set; }
+        public int Minutes { get; set; } 
+        //public int ProgressId { get; set; }
+        public Progress Progress { get; set; }
+        public int ScheduleId { get; set; }
+        public Schedule Schedule { get; set; }
+        
+    }
+}
